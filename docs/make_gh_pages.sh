7133cf7c6f0c89cb7d229894626095fa7b19e8c9
Artemy Kolchinsky <akolchin@indiana.edu>
2013-11-10 18:39:43 -0500

    Documentation updated

---
+++ docs/make_gh_pages.sh
@@ -0,0 +1,20 @@
+make clean
+mkdir ../htmldocs/html
+
+cd ../htmldocs/html
+git clone git@github.iu.edu:akolchin/DynPy.git ..
+git checkout origin/gh-pages -b gh-pages
+git branch -d master
+
+cd ../../docs
+
+make html
+
+echo "Created by following https://gist.github.com/chrisjacob/833223" > README.textile
+git add .
+git commit -m "Documentation updated"
+
+git push origin gh-pages
+
+cd ../../docs
+
