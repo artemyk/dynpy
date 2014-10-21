558d32980e49ec6d34a416a86711f22fc709b7f6
Artemy Kolchinsky <akolchin@indiana.edu>
2014-10-20 21:12:23 -0400

    Added test

---
+++ tests/test_base.py
@@ -0,0 +1,21 @@
+from __future__ import division, print_function, absolute_import
+import six
+range = six.moves.range
+
+import numpy as np
+import scipy.sparse as ss
+import dynpy
+
+class ExampleSystem(dynpy.dynsys.DiscreteStateDynamicalSystem):
+
+	def states(self):
+		return [1,2]
+
+	def _iterate_1step_discrete(self, x):
+		return 2 if x == 1 else 2
+
+
+def test_dynsys():
+	d = ExampleSystem()
+	print(d)
+	d.print_attractor_basins()
