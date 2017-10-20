3e66198a999af27b65c6573ac9ac76daab5c7882
Artemy Kolchinsky <artemyk@gmail.com>
2017-10-19 22:19:23 -0600

    Fix sphinx pngmath error

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.pngmath',
+    'sphinx.ext.imgmath',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
