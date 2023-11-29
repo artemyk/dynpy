d7461a4bf392e79d9479f8937f344a5760695e29
Artemy Kolchinsky <artemyk@gmail.com>
2023-11-29 10:09:30 +0100

    Update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'dynpy'
-copyright = u'2013-2014, Artemy Kolchinsky'
+copyright = u'2013-2023, Artemy Kolchinsky'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
